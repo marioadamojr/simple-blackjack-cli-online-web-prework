b5468112b53488388b5d610d23a901ab87a205ca
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-03 01:15:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,9 +50,8 @@ end
 #####################################################
 
 def runner
-  # code runner here
   welcome
-  initial_round
+  card_total = initial_round
   until card_total > 21
       card_total = hit?(card_total)
       display_card_total(card_total)
